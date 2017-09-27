6c860562d6076cf5ace358d6e78555965f61da45
Ondrej Prazak <oprazak@redhat.com>
2017-09-27 13:53:48 +0200

    wip

--- webpack/index.js
+++ webpack/index.js
@@ -2,10 +2,10 @@ import componentRegistry from 'foremanReact/components/componentRegistry';
 import OpenscapPieChart from './OpenscapPieChart';
 import openscapReducer from './reducers';
 import Policies from './components/policies';
-import { injectReducer } from 'foremanReact/redux';
+// import { injectReducer } from 'foremanReact/redux';
 
 componentRegistry.registerMultiple([{ name: 'OpenscapPieChart', type: OpenscapPieChart },
                                     { name: 'Policies', type: Policies },
                                    ]);
 
-injectReducer('oscap', openscapReducer);
+// injectReducer('oscap', openscapReducer);
