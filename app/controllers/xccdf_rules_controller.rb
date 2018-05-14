575fdfa8a56d14376f9f923c2d1cea0796a2f444
Ondrej Prazak <oprazak@redhat.com>
2018-05-14 17:49:38 +0200

    wip

--- app/controllers/xccdf_rules_controller.rb
+++ app/controllers/xccdf_rules_controller.rb
@@ -1,4 +1,8 @@
 class XccdfRulesController < ApplicationController
   def index
+    logs = Log.by_report_type(ForemanOpenscap::ArfReport).uniq(&:source_id)
+    @res = logs.map do |log|
+      { :source => log.source, :message => log.message }
+    end
   end
 end
