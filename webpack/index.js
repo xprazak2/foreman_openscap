33bfb3eb76f7ef0523cdac7b2474c775c5f6242f
Ondrej Prazak <oprazak@redhat.com>
2018-05-14 13:34:45 +0200

    wip

--- webpack/index.js
+++ webpack/index.js
@@ -2,13 +2,17 @@ import componentRegistry from 'foremanReact/components/componentRegistry';
 import OpenscapPieChart from './OpenscapPieChart';
 import openscapReducer from './reducers';
 import Policies from './components/policies';
-import { injectReducer } from 'redux-injector';
+import XccdfRules from './components/XccdfRules';
+// import { injectReducer } from 'redux-injector';
 // import { registerReducer } from 'foremanReact/common/MountingService';
+
+console.log('hola')
 
 componentRegistry.registerMultiple([{ name: 'OpenscapPieChart', type: OpenscapPieChart },
                                     { name: 'Policies', type: Policies },
+                                    { name: 'XccdfRules', type: XccdfRules }
                                    ]);
 
 // injectReducer('hosts.content', openscapReducer);
 
-injectReducer('hosts.content', openscapReducer);
+// injectReducer('hosts.content', openscapReducer);
